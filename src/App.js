c361c6e1f504635c4b25c25dbd693e303470c686
Peter Edbrooke <peter.john.edbrooke@gmail.com>
2022-03-23 01:48:16 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import styles from "./App.module.scss";
-import axios from "axios";
+import styles from "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route>
-            <Route index element={<Dashboard />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Dashboard></Dashboard>
     </>
   );
 };
